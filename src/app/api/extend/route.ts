22c8c4179d848285663912419befbbb3200349f3
gb <13611993+netalkGB@users.noreply.github.com>
2024-01-26 00:04:59 +0900

    Set status to 401 for expired sessions

--- src/app/api/extend/route.ts
+++ src/app/api/extend/route.ts
@@ -4,6 +4,11 @@ import { NextResponse } from 'next/server'
 
 export async function POST (): Promise<NextResponse> {
   const session = await getSession()
+  if (session.tokens?.token?.accessToken === undefined || session.tokens?.idToken === undefined) {
+    return NextResponse.json({ message: 'error' }, {
+      status: 401
+    })
+  }
   await updateAccessToken(session)
   await session.save()
   return NextResponse.json({ message: 'ok' }, { status: 200 })
